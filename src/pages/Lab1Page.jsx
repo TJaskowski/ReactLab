fdf173f67b542fa12d20dd17806d556845027634
Tomasz <116389458+TJaskowski@users.noreply.github.com>
2024-10-20 22:22:34 +0200

    First commit

---
+++ src/pages/Lab1Page.jsx
@@ -0,0 +1,18 @@
+import {data} from "../data/module-data";
+
+const NameItem = ({name, id}) => <li key={id}>{name}</li>;
+
+function Lab1Page() {
+    return ( 
+        <>
+        <h1>Laboratorium 1</h1>
+        <ul>
+            {data.map((person, i) => 
+            <NameItem name={person.name} id={person.id} key={i}/>
+            )}
+        </ul>
+        </>
+     );
+}
+
+export default Lab1Page;
