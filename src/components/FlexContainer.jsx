3bb229b5798bb8c067edb867dfbe4b2bb6e4674d
Tomasz J <tomasz.jaskowski@microsoft.wsei.edu.pl>
2024-11-12 22:12:45 +0100

    lab 3 zadanie 6

--- src/components/FlexContainer.jsx
+++ src/components/FlexContainer.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import { useReducer } from 'react'
+import AppReducer from '../data/AppReducer'
 
 function FlexContainer({element: Element, data}) {
+    const [items, dispatch] = useReducer(AppReducer, data);
   return (
     <Container>
       <Row>
-        {data.map((item) =>
+        {items.map((item) =>
             <Col  className='d-flex justify-content-center mb-4'>
                 <Element key={item.id} {...item}/>
             </Col>
---
+++ src/data/AppReducer.js
@@ -0,0 +1,11 @@
+export default function AppReducer(state, action){
+    switch(action.type){
+        case "edit":
+            return state;
+        case "rate":{
+            return state;
+        }
+        case "delete":
+            return state;       
+    }
+}   
